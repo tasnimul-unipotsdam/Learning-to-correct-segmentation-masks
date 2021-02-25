b1e1a49e5f4717b766b1b0311d2b465f8dba8024
Md Tasnimul Hasan <tasnimul.abir@gmail.com>
2021-02-25 13:00:02 +0100

    add steps

--- training_unet.py
+++ training_unet.py
@@ -9,7 +9,8 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 devises = tf.config.experimental.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(devises[0], True)
 
-train_dataset = TFRecordReader("D:/PROJECTS/internship/records", is_training=True).train_dataset()
+train_dataset = TFRecordReader("D:/PROJECTS/internship/records",
+                               is_training=True).train_dataset()
 validation_dataset = TFRecordReader("D:/PROJECTS/internship/records",
                                     is_training=False).validation_dataset()
 
@@ -24,14 +25,19 @@ def compile_model():
 
 
 def train_model(model):
+    BATCH_SIZE = 4
+    STEPS_PER_EPOCH = 1016 // BATCH_SIZE
+    VALIDATION_STEPS = 254 // BATCH_SIZE
     history = model.fit(train_dataset,
                         verbose=2,
-                        epochs=10,
+                        epochs=100,
+                        steps_per_epoch=STEPS_PER_EPOCH,
+                        validation_steps=VALIDATION_STEPS,
                         validation_data=validation_dataset,
                         workers=12)
 
-    model.save("unet_4_model.h5")
-    plot_dice_loss_coefficient(history, "unet_4_dice_loss_coefficient.jpg")
+    model.save("unet_6_model.h5")
+    plot_dice_loss_coefficient(history, "unet_6_dice_loss_coefficient.jpg")
 
 
 if __name__ == '__main__':
