caf59434bc15da4e93aea82e13e3379cbc52af34
Md Tasnimul Hasan <tasnimul.abir@gmail.com>
2021-04-01 15:12:11 +0200

    add surface distance metrics

--- model/metrics_optimizer.py
+++ model/metrics_optimizer.py
@@ -1,12 +1,95 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 import tensorflow as tf
+from tensorflow import keras
 import tensorflow.keras.backend as K
+
+
+from distance import compute_surface_distances, compute_average_surface_distance, \
+    compute_robust_hausdorff
 
 smooth = 1
 
 
-def generalized_dice_coefficient(y_true, y_pred):
+def surface_distance(mask_gt, mask_pred):
+    mask_gt_cp = mask_gt.numpy().copy()
+    mask_pred_cp = mask_pred.numpy().copy()
+
+    sf_list = []
+    for i in range(mask_gt.shape[0]):
+        m_gt = mask_gt_cp[i]
+        p_gt = mask_pred_cp[i]
+
+        m_gt = np.asarray(m_gt, np.float32)
+        m_gt = m_gt >= 0.5
+        m_gt = np.resize(m_gt, (mask_gt.shape[1], mask_gt.shape[2]))
+
+        p_gt = np.asarray(p_gt, np.float32)
+        p_gt = p_gt >= 0.5
+        p_gt = np.resize(p_gt, (mask_pred.shape[1], mask_pred.shape[2]))
+
+        surface_dict = compute_surface_distances(m_gt, p_gt, spacing_mm=(1, 1))
+        sf = compute_average_surface_distance(surface_dict)
+        sf_list.append([sf[0], sf[1]])
+
+    sum_arr = np.array(sf_list).sum(axis=0)
+    avg = sum_arr / 4
+    return tuple(avg)
+
+
+# def surface_distance(mask_gt, mask_pred):
+#     mask_gt = np.asarray(mask_gt, np.float32)
+#     mask_gt = mask_gt >= 0.5
+#     mask_gt = np.resize(mask_gt, (mask_gt.shape[1], mask_gt.shape[2]))
+#
+#     mask_pred = np.array(mask_pred, np.float32)
+#     mask_pred = mask_pred >= 0.5
+#     mask_pred = np.resize(mask_pred, (mask_pred.shape[1], mask_pred.shape[2]))
+#
+#     surface_dict = compute_surface_distances(mask_gt, mask_pred, spacing_mm=(1, 1))
+#     return compute_average_surface_distance(surface_dict)
+
+
+def robust_hausdorff(mask_gt, mask_pred):
+    mask_gt_cp = mask_gt.numpy().copy()
+    mask_pred_cp = mask_pred.numpy().copy()
+
+    haus_dorff_dist = []
+    for i in range(mask_gt.shape[0]):
+        m_gt = mask_gt_cp[i]
+        p_gt = mask_pred_cp[i]
+
+        m_gt = np.asarray(m_gt, np.float32)
+        m_gt = m_gt >= 0.5
+        m_gt = np.resize(m_gt, (mask_gt.shape[1], mask_gt.shape[2]))
+
+        p_gt = np.asarray(p_gt, np.float32)
+        p_gt = p_gt >= 0.5
+        p_gt = np.resize(p_gt, (mask_pred.shape[1], mask_pred.shape[2]))
+
+        surface_dict = compute_surface_distances(m_gt, p_gt, spacing_mm=(1, 1))
+        haus_dorff_dist.append(compute_robust_hausdorff(surface_dict, 100))
+
+    avg = sum(haus_dorff_dist) / 4
+    return avg
+
+
+# def robust_hausdorff(mask_gt, mask_pred):
+#
+#     mask_gt = np.asarray(mask_gt, np.float32)
+#     mask_gt = mask_gt >= 0.5
+#     mask_gt = np.resize(mask_gt, (mask_gt.shape[1], mask_gt.shape[2]))
+#
+#     mask_pred = np.array(mask_pred, np.float32)
+#     mask_pred = mask_pred >= 0.5
+#     mask_pred = np.resize(mask_pred, (mask_pred.shape[1], mask_pred.shape[2]))
+#
+#     surface_dict = compute_surface_distances(mask_gt, mask_pred, spacing_mm=(1, 1))
+#     haus_dorff_dist = compute_robust_hausdorff(surface_dict, 100)
+#     return haus_dorff_dist
+
+
+def dice_coefficient(y_true, y_pred):
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
@@ -15,21 +98,32 @@ def generalized_dice_coefficient(y_true, y_pred):
 
 
 def dice_loss(y_true, y_pred):
-    loss = 1 - generalized_dice_coefficient(y_true, y_pred)
+    loss = 1 - dice_coefficient(y_true, y_pred)
     return loss
 
 
-def log_cosh_dice_loss(y_true, y_pred):
-    x = dice_loss(y_true, y_pred)
-    return tf.math.log((tf.exp(x) + tf.exp(-x)) / 2.0)
-
+binary_crossentropy = tf.keras.losses.BinaryCrossentropy(from_logits=False)
 
 adam = tf.keras.optimizers.Adam(learning_rate=0.0001)
 
 
-def plot_dice_loss_coefficient(history, name):
+def plot_crossentropy_loss(history, name):
     fig = plt.figure()
-    plt.subplot(1, 2, 1)
+
+    plt.plot(history.history['loss'], color='b', label='train_loss')
+    plt.plot(history.history['val_loss'], color='r', label='validation_loss')
+    plt.title('crossentropy_loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'])
+
+    plt.show()
+    fig.savefig(name)
+
+
+def plot_dice_loss(history, name):
+    fig = plt.figure()
+
     plt.plot(history.history['loss'], color='b', label='train_loss')
     plt.plot(history.history['val_loss'], color='r', label='validation_loss')
     plt.title('dice loss')
@@ -37,16 +131,39 @@ def plot_dice_loss_coefficient(history, name):
     plt.xlabel('epoch')
     plt.legend(['train', 'validation'])
 
-    plt.subplot(1, 2, 2)
-    plt.plot(history.history['generalized_dice_coefficient'], color='b', label="train_coefficient")
-    plt.plot(history.history['val_generalized_dice_coefficient'], color='r',
-             label="validation_coefficient")
-    plt.title('dice coefficient')
-    plt.ylabel('coefficient')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'validation'])
     plt.show()
     fig.savefig(name)
 
 
+def plot_metrics(history, name):
+    fig = plt.figure(num=None, figsize=(20, 6), dpi=150, facecolor='w', edgecolor='k',
+                     tight_layout=True)
 
+    plt.subplot(1, 3, 1)
+    plt.plot(history.history['dice_coefficient'], color='b', label="train_coefficient")
+    plt.plot(history.history['val_dice_coefficient'], color='r', label="validation_coefficient")
+    plt.title('dice coefficient')
+    plt.ylabel('coefficient')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'])
+
+    plt.subplot(1, 3, 2)
+    plt.plot(history.history['surface_distance'], color='b', label="train_surface_distance")
+    plt.plot(history.history['val_surface_distance'], color='r',
+             label="validation_surface_distance")
+    plt.title('surface distance')
+    plt.ylabel('surface_distance')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'])
+
+    plt.subplot(1, 3, 3)
+    plt.plot(history.history['robust_hausdorff'], color='b', label="train_robust_hausdorff")
+    plt.plot(history.history['val_robust_hausdorff'], color='r',
+             label="validation_robust_hausdorff")
+    plt.title('hausdorff distance')
+    plt.ylabel('hausdorff_distance')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'validation'])
+
+    plt.show()
+    fig.savefig(name)
--- model/unet.py
+++ model/unet.py
@@ -1,4 +1,8 @@
 import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import *
+
+from tensorflow.keras.regularizers import l2
 
 
 def conv_layer(x, num_filters, kernel_size, padding='same'):
@@ -32,20 +36,23 @@ def unet():
     conv4 = conv_layer(pool3, num_filters=512, kernel_size=3)
     conv4 = conv_layer(conv4, num_filters=512, kernel_size=3)
 
-    conc1 = tf.keras.layers.concatenate([tf.keras.layers.Conv2DTranspose(256, 2, strides=2, padding="same")
-                                         (conv4), conv3], axis=3)
+    conc1 = tf.keras.layers.concatenate(
+        [tf.keras.layers.Conv2DTranspose(256, 2, strides=2, padding="same")
+         (conv4), conv3], axis=3)
 
     conv5 = conv_layer(conc1, num_filters=256, kernel_size=3)
     conv5 = conv_layer(conv5, num_filters=256, kernel_size=3)
 
-    conc2 = tf.keras.layers.concatenate([tf.keras.layers.Conv2DTranspose(128, 2, strides=2, padding="same")
-                                         (conv5), conv2], axis=3)
+    conc2 = tf.keras.layers.concatenate(
+        [tf.keras.layers.Conv2DTranspose(128, 2, strides=2, padding="same")
+         (conv5), conv2], axis=3)
 
     conv6 = conv_layer(conc2, num_filters=128, kernel_size=3)
     conv6 = conv_layer(conv6, num_filters=128, kernel_size=3)
 
-    conc3 = tf.keras.layers.concatenate([tf.keras.layers.Conv2DTranspose(64, 2, strides=2, padding="same")
-                                         (conv6), conv1], axis=3)
+    conc3 = tf.keras.layers.concatenate(
+        [tf.keras.layers.Conv2DTranspose(64, 2, strides=2, padding="same")
+         (conv6), conv1], axis=3)
 
     conv7 = conv_layer(conc3, num_filters=64, kernel_size=3)
     conv7 = conv_layer(conv7, num_filters=64, kernel_size=3)
@@ -57,8 +64,61 @@ def unet():
     return unet_model
 
 
+def get_unet():
+    inputs = Input(shape=(384, 160, 1))
+
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool3)
+    conv4 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(pool4)
+    conv5 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv5)
+
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4],
+                      axis=3)
+
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(up6)
+    conv6 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv6)
+
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3],
+                      axis=3)
+
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(up7)
+    conv7 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv7)
+
+    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2],
+                      axis=3)
+
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(up8)
+    conv8 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv8)
+
+    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1],
+                      axis=3)
+
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(up9)
+    conv9 = Conv2D(32, (3, 3), activation='relu', padding='same')(conv9)
+
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid', kernel_regularizer=l2(0.01))(conv9)
+
+    new_model = Model(inputs=inputs, outputs=conv10)
+
+    return new_model
+
+
 if __name__ == '__main__':
-    model = unet()
+    model = get_unet()
     model.summary()
 
     # dot_img_file = 'model_1.png'
