ec8eabccb1f6309b2f020e591021282417815b9c
Md Tasnimul Hasan <tasnimul.abir@gmail.com>
2021-06-11 01:59:09 +0200

    add(mcdp_3D): add dropout after every conv block and change the rate

--- MCDP_3D/model_mcdp_3D.py
+++ MCDP_3D/model_mcdp_3D.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def conv_layer_3D(x, num_filters, kernel_size, padding='same'):
+def conv_layer_3D(x, num_filters, kernel_size, padding='same', drop_rate=0.025, dropout=True):
     x = tf.keras.layers.Conv3D(num_filters,
                                kernel_size=kernel_size,
                                padding=padding)(x)
@@ -9,50 +9,43 @@ def conv_layer_3D(x, num_filters, kernel_size, padding='same'):
 
     x = tf.keras.layers.ReLU()(x)
 
+    x = tf.keras.layers.Dropout(drop_rate)(x, training=dropout)
+
     return x
 
 
-def unet_mcdp_3D(rate=0.5, dropout=True):
+def unet_mcdp_3D():
     inputs = tf.keras.Input(shape=(128, 224, 224, 1))
     x = inputs
     conv1 = conv_layer_3D(x, num_filters=4, kernel_size=3)
     conv1 = conv_layer_3D(conv1, num_filters=4, kernel_size=3)
-    drop1 = tf.keras.layers.Dropout(rate)(conv1, training=dropout)
-    pool1 = tf.keras.layers.MaxPool3D()(drop1)
+    pool1 = tf.keras.layers.MaxPool3D()(conv1)
 
     conv2 = conv_layer_3D(pool1, num_filters=8, kernel_size=3)
     conv2 = conv_layer_3D(conv2, num_filters=8, kernel_size=3)
-    drop2 = tf.keras.layers.Dropout(rate)(conv2, training=dropout)
-    pool2 = tf.keras.layers.MaxPool3D()(drop2)
+    pool2 = tf.keras.layers.MaxPool3D()(conv2)
 
     conv3 = conv_layer_3D(pool2, num_filters=16, kernel_size=3)
     conv3 = conv_layer_3D(conv3, num_filters=16, kernel_size=3)
-    drop3 = tf.keras.layers.Dropout(rate)(conv3, training=dropout)
-    pool3 = tf.keras.layers.MaxPool3D()(drop3)
+    pool3 = tf.keras.layers.MaxPool3D()(conv3)
 
     conv4 = conv_layer_3D(pool3, num_filters=32, kernel_size=3)
     conv4 = conv_layer_3D(conv4, num_filters=32, kernel_size=3)
 
-    drop4 = tf.keras.layers.Dropout(rate)(conv4, training=dropout)
-
     concat1 = tf.keras.layers.concatenate(
-        [tf.keras.layers.Conv3DTranspose(16, 2, strides=2, padding="same")(drop4), conv3])
+        [tf.keras.layers.Conv3DTranspose(16, 2, strides=2, padding="same")(conv4), conv3])
 
     conv5 = conv_layer_3D(concat1, num_filters=16, kernel_size=3)
     conv5 = conv_layer_3D(conv5, num_filters=16, kernel_size=3)
 
-    drop5 = tf.keras.layers.Dropout(rate)(conv5, training=dropout)
-
     concat2 = tf.keras.layers.concatenate(
-        [tf.keras.layers.Conv3DTranspose(8, 2, strides=2, padding="same")(drop5), conv2])
+        [tf.keras.layers.Conv3DTranspose(8, 2, strides=2, padding="same")(conv5), conv2])
 
     conv6 = conv_layer_3D(concat2, num_filters=8, kernel_size=3)
     conv6 = conv_layer_3D(conv6, num_filters=8, kernel_size=3)
 
-    drop6 = tf.keras.layers.Dropout(rate)(conv6, training=dropout)
-
     concat3 = tf.keras.layers.concatenate(
-        [tf.keras.layers.Conv3DTranspose(4, 2, strides=2, padding="same")(drop6), conv1])
+        [tf.keras.layers.Conv3DTranspose(4, 2, strides=2, padding="same")(conv6), conv1])
 
     conv7 = conv_layer_3D(concat3, num_filters=4, kernel_size=3)
     conv7 = conv_layer_3D(conv7, num_filters=4, kernel_size=3)
